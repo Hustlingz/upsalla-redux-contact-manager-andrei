a818b46efa66cf307a4239453dbaf5f25b9afeef
Andrei Mares <andreimaresh@gmail.com>
2024-03-05 21:18:42 +0400

    920

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,18 +1,26 @@
+// disclaimer: better in tailwind
 const skins = {
-    primary: 'bg-blue-500',
+  primary:
+    'bg-blue-500 inline-flex items-center h-full text-center py-2 px-4 text-white rounded hover:bg-blue-700',
+  success:
+    'bg-green-500 inline-flex items-center h-full text-center py-2 px-4 text-white rounded hover:bg-green-700',
+  danger:
+    'bg-red-500 inline-flex items-center h-full text-center py-2 px-4 text-white rounded hover:bg-red-700',
 };
 
 export const Button = ({
-    element = 'button',
-    children,
-    skin = 'primary',
-    className = '',
-    ...props
+  element = 'button',
+  children,
+  skin = 'primary',
+  className = '',
+  ...props
 }) => {
-    const skinClasses = skins[skin];
-    const Element = element;
+  const skinClasses = skins[skin];
+  const Element = element;
 
-    return (
-        <Element className={`${skinClasses} ${className}`} {...props}>{children}</Element>
-    );
+  return (
+    <Element className={`${skinClasses} ${className}`} {...props}>
+      {children}
+    </Element>
+  );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { Provider as ReduxProvider} from 'react-redux';
-import store from './store'
+import { Provider as ReduxProvider } from 'react-redux';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
